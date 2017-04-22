24f27087621ea012f798ef5d168dfb402f65ea13
Kevin Cooper <cooperka92@gmail.com>
2017-04-22 12:13:07 -0400

    :package: Change all directory references {Dev => dev}

--- aliases/misc.zsh
+++ aliases/misc.zsh
@@ -26,7 +26,7 @@ alias v='xclip -o'
 # Memory jogs
 alias lines='echo "cat -n FILE"'
 alias fix-monitor='echo "http://www.ireckon.net/2013/03/force-rgb-mode-in-mac-os-x-to-fix-the-picture-quality-of-an-external-monitor"; echo "/System/Library/Displays/Overrides/DisplayVendorID-469/DisplayProductID-22fd"'
-alias fix-eclipse='echo "rm ~/Dev/Workspaces/EclipseAndroid/.metadata/.plugins/org.eclipse.e4.workbench/workbench.xmi"'
+alias fix-eclipse='echo "rm ~/dev/Workspaces/EclipseAndroid/.metadata/.plugins/org.eclipse.e4.workbench/workbench.xmi"'
 alias fix-xcode='echo "rm -rf /Users/klap-hotel/Library/Developer/Xcode/DerivedData"'
 ios-dir() {
   echo 'Pause simulator, then `po NSHomeDirectory()`'
