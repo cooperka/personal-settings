23840e6e46f8efed89bf987b6e9623617abd155a
Kevin Cooper <cooperka92@gmail.com>
2017-12-06 15:17:39 -0500

    :wrench: Add paths for Go

--- aliases/ubuntu.zsh
+++ aliases/ubuntu.zsh
@@ -1,11 +1,11 @@
 # Misc. paths
-addpath ~/sdk/redis-3.2.3/src
-addpath ~/Applications/anaconda3/bin
+addpath "$HOME/sdk/redis-3.2.3/src"
+addpath "$HOME/Applications/anaconda3/bin"
 
 # WebStorm
 # https://youtrack.jetbrains.com/issue/IDEA-78860
 export IBUS_ENABLE_SYNC_MODE=1
-export WS_DIR="~/Applications/WebStorm-11"
+export WS_DIR="$HOME/Applications/WebStorm-11"
 alias ws="sh $WS_DIR/bin/webstorm.sh"
 
 # Android
@@ -15,3 +15,8 @@ alias and="sh $AS_DIR/bin/studio.sh"
 # Ruby
 export PATH="$HOME/.rbenv/bin:$PATH" # Ruby shims must come first in path
 eval "$(rbenv init -)"
+
+# Go
+export GOPATH="$HOME/sdk/go"
+addpath "$GOROOT/bin"
+addpath "$GOPATH/bin"
