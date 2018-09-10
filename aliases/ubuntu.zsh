6909a68784c6e1bbad0477276f4b64bd3090b2a2
Kevin Cooper <cooperka92@gmail.com>
2018-09-10 17:25:24 -0400

    :wrench: Update ubuntu aliases for renamed sdk => sdks

--- aliases/ubuntu.zsh
+++ aliases/ubuntu.zsh
@@ -1,6 +1,6 @@
 # Misc. paths
-addpath "$HOME/sdk/redis-3.2.3/src"
-addpath "$HOME/Applications/anaconda3/bin"
+addpath "$HOME/sdks/redis-3.2.3/src"
+addpath "$HOME/apps/anaconda3/bin"
 addpath "$HOME/.local/bin"
 
 # Disable fwd/back on J-Tech Digital mouse at home.
@@ -11,16 +11,16 @@ disable-mouse-extras() {
 disable-mouse-extras
 
 # Python
-alias python="$HOME/Applications/anaconda3/bin/python"
+alias python="$HOME/apps/anaconda3/bin/python"
 
 # WebStorm
 # https://youtrack.jetbrains.com/issue/IDEA-78860
 export IBUS_ENABLE_SYNC_MODE=1
-export WS_DIR="$HOME/Applications/WebStorm-11"
+export WS_DIR="$HOME/apps/WebStorm-11"
 alias ws="sh $WS_DIR/bin/webstorm.sh"
 
 # Android
-export AS_DIR=~/Applications/android-studio
+export AS_DIR="$HOME/apps/android-studio"
 alias and="sh $AS_DIR/bin/studio.sh"
 
 # Ruby
@@ -28,6 +28,6 @@ export PATH="$HOME/.rbenv/bin:$PATH" # Ruby shims must come first in path
 eval "$(rbenv init -)"
 
 # Go
-export GOPATH="$HOME/sdk/go"
+export GOPATH="$HOME/sdks/go"
 addpath "$GOROOT/bin"
 addpath "$GOPATH/bin"
