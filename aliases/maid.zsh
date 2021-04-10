63211cf4555a6250dd233fbe93a5a3a52dc49959
Kevin Cooper <cooperka92@gmail.com>
2021-04-10 12:40:53 -0700

    :package: Rename maid => safe

--- aliases/maid.zsh
+++
@@ -1,5 +0,0 @@
-addpath '/Users/klap-hotel/.safe/cli'
-
-export MAID_DIR=~/dev/maidsafe
-
-alias maid='cd ${MAID_DIR}'
---
+++ aliases/safe.zsh
@@ -0,0 +1,6 @@
+addpath '/Users/klap-hotel/.safe/cli'
+
+export SAFENET_DIR=~/dev/safenet
+
+alias safenet='cd ${SAFENET_DIR}'
+alias maid=safenet
