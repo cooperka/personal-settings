682f034c06be7c6da55cf9ec7df643c0cf56ce23
Kevin Cooper <cooperka92@gmail.com>
2021-04-10 12:10:54 -0700

    Various tweaks

--- aliases/mac.zsh
+++ aliases/mac.zsh
@@ -1,4 +1,5 @@
 addpath '/Users/klap-hotel/Library/Python/2.7/bin'
+source "$HOME/.cargo/env"
 
 # `brew install coreutils` first.
 alias readlink=greadlink
--- aliases/maid.zsh
+++ aliases/maid.zsh
@@ -1,3 +1,5 @@
+addpath '/Users/klap-hotel/.safe/cli'
+
 export MAID_DIR=~/dev/maidsafe
 
 alias maid='cd ${MAID_DIR}'
