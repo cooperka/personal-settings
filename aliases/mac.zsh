9cf8368b4bb12aed90ed743096def4f7141c9356
Kevin Cooper <cooperka92@gmail.com>
2021-07-15 21:44:22 -0700

    :wrench: Add new JAVA_HOME on mac

--- aliases/mac.zsh
+++ aliases/mac.zsh
@@ -1,5 +1,8 @@
 addpath '/Users/klap-hotel/Library/Python/2.7/bin'
 source "$HOME/.cargo/env"
 
+# JDK installed via homebrew
+export JAVA_HOME='/Library/Java/JavaVirtualMachines/openjdk.jdk/Contents/Home'
+
 # `brew install coreutils` first.
 alias readlink=greadlink
