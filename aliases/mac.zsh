a65cdb9c1af19af2a6606dd2680fce4735834f0b
Kevin Cooper <cooperka92@gmail.com>
2021-10-20 19:47:10 -0700

    :wrench: Update java location

--- aliases/mac.zsh
+++ aliases/mac.zsh
@@ -1,8 +1,8 @@
 addpath '/Users/klap-hotel/Library/Python/2.7/bin'
 source "$HOME/.cargo/env"
 
-# JDK installed via homebrew
-export JAVA_HOME='/Library/Java/JavaVirtualMachines/openjdk.jdk/Contents/Home'
+# Default JDK path.
+export JAVA_HOME='/Library/Java/JavaVirtualMachines/jdk1.8.0_91.jdk/Contents/Home'
 
 # `brew install coreutils` first.
 alias readlink=greadlink
