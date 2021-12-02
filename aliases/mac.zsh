05e0104a23cb6effd82478f35729973b1d005f75
Kevin Cooper <cooperka92@gmail.com>
2021-12-02 14:05:07 -0800

    :wrench: Update JDK

--- aliases/mac.zsh
+++ aliases/mac.zsh
@@ -2,7 +2,7 @@ addpath '/Users/klap-hotel/Library/Python/2.7/bin'
 source "$HOME/.cargo/env"
 
 # Default JDK path.
-export JAVA_HOME='/Library/Java/JavaVirtualMachines/jdk1.8.0_91.jdk/Contents/Home'
+export JAVA_HOME='/Library/Java/JavaVirtualMachines/jdk-11.0.13.jdk/Contents/Home'
 
 # Major speedup + don't break old dependencies
 export HOMEBREW_NO_AUTO_UPDATE=1
